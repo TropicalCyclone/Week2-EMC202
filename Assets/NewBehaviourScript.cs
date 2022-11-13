58accbf6a45876571faac9529fac89ea9e475072
ZEPHYRUS-G14\Kyle Logdat <logdatkyle@gmail.com>
2022-11-13 13:23:11 +0800

    added new variable to script

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    [SerializeField] private float amogus;
     // Start is called before the first frame update
     void Start()
     {
