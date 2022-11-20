0caddf5d7b4c9ee7d7a5810953cc27a5bbc38b3e
ZEPHYRUS-G14\Kyle Logdat <logdatkyle@gmail.com>
2022-11-20 17:28:42 +0800

    Added Base Player Class Script

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,55 +1,79 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player
 {
-    private  int hitPoints;
-    public int strength;
-    public int speed;
-    private int defence;
-    private int fireRate;
-    public int ammoCapacity;
+    protected string _name;
+    private float _health;
+    protected float _strength;
+    protected float _speed;
+    protected float _defence;
+    protected float _fireRate;
+    protected int _ammoCapacity;
 
-    public void damagePlayer(int dam)
+    public string Name
     {
-        hitPoints -= dam;
+        get => _name;
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                _name = "Steve";
+            }
+            else
+            {
+                _name = value;
+            }
+        }
     }
 
     public Player()
     {
-        hitPoints = 100;
-        strength = 1;  
-        speed = 1;
-        defence = 1;
-        fireRate = 1;
-        ammoCapacity = 10;
+        _name = "";
+        _health = 100.0f;
+        _strength = 1.0f;
+        _speed = 1.0f;
+        _defence = 1.0f;
+        _fireRate = 1.0f;
+        _ammoCapacity = 10;
     }
 
-    public Player(int HP,int STR,int SP, int DEF, int rate, int ammoCap)
+    public Player(float STR, float SP, float DEF, float rate, int ammoCap)
     {
-        HP = hitPoints;
-        STR = strength;
-        SP = speed;
-        DEF = defence;
-        rate = fireRate;
-        ammoCap = ammoCapacity;
+        STR = _strength;
+        SP = _speed;
+        DEF = _defence;
+        rate = _fireRate;
+        ammoCap = _ammoCapacity;
     }
 
-    public int getHitpoints()
+    public void damagePlayer(int dam)
     {
-        return hitPoints;
+        _health -= dam;
     }
 
-    public int getFirerate()
+    public float getHitpoints()
     {
-        return fireRate;
+        return _health;
+    }
+
+    public float getFirerate()
+    {
+        return _fireRate;
     }
     public void ammoReduce(int Rate)
     {
-        while(ammoCapacity > 0)
+        while (_ammoCapacity > 0)
         {
-            ammoCapacity -= Rate;
+            _ammoCapacity -= Rate;
         }
+    }
+
+    public string GetProperties()
+    {
+        var properties = $"{nameof(_health)}:{_health},{nameof(_defence)}:{_defence},{nameof(_fireRate)}:{_fireRate},{nameof(_speed)}:{_speed},{nameof(_strength)}:{_strength},{nameof(_ammoCapacity)}:{_ammoCapacity},";
+        return properties;
     }
 }
